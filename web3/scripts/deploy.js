0cab985f80c28183aea32f5d1e75a502c90e5078
siddhantsingh01 <siddhantrajawat07@gmail.com>
2026-04-29 14:29:21 +0530

    Initial commit

---
+++ web3/scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
+  const crowdfunding = await CrowdFunding.deploy();
+
+  await crowdfunding.deployed();
+
+  console.log("Contract deployed to:", crowdfunding.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
